ab35f7b55fdafd5613f568a042118f7905faaa22
Johann Frei <johann.frei@informatik.uni-augsburg.de>
2021-08-03 01:20:58 +0200

    Fix update loop

--- dockerized/wikidata-loader/entrypoint.sh
+++ dockerized/wikidata-loader/entrypoint.sh
@@ -4,12 +4,15 @@
 echo "Waiting for blazegraph to be available..."
 /waitForBlazegraph.sh
 
-# Detect enabled update loop
-if [ -f "/.doUpdate"]; then
-    # Async update loop
-    echo "UpdateEntrypoint: Start update loop"
-    echo "Trigger at: $(date)" >> /updateEntrypoint.log
-    /bin/bash /updateEntrypoint.sh >> /updateEntrypoint.log 2>&1 &
+# Detect initial startup
+if [ "$1" = "/usr/bin/sleep" ] && [ "$2" = "infinity" ]; then
+    # Detect enabled update loop
+    if [ -f "/.doUpdate"]; then
+        # Async update loop
+        echo "UpdateEntrypoint: Start update loop"
+        echo "Trigger at: $(date)" >> /updateEntrypoint.log
+        /bin/bash /updateEntrypoint.sh >> /updateEntrypoint.log 2>&1 &
+    fi
 fi
 
 exec "$@"
--- dockerized/wikidata-loader/scripts/05_enableRunUpdateLoop.sh
+++ dockerized/wikidata-loader/scripts/05_enableRunUpdateLoop.sh
@@ -3,6 +3,7 @@
 if [ ! -f "/.doUpdate" ]; then
     echo "Enabling update loop.."
     touch "/.updatedData"
+    echo "Please restart the container (but do NOT delete it)"
 else
     echo "Update loop already enabled."
 fi
