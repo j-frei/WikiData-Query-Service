10d7905eeab87971d5386db914bd7241ffd589dc
Johann Frei <johann.frei@informatik.uni-augsburg.de>
2021-08-02 23:19:27 +0200

    Fix typo

--- dockerized/wikidata-loader/updateScript.sh
+++ dockerized/wikidata-loader/updateScript.sh
@@ -2,7 +2,7 @@
 
 while true; do
     echo "Loop: Run updater script at $(date)..."
-    /wikidata/service/runUpdate.sh -n wdq -h http://query-service:9999 -l "$WIKI_LANG" -S | true
+    /wikidata/service/runUpdate.sh -n wdq -h http://query-service:9999 -l "$WIKI_LANG" -S || true
     echo "Update script done... Sleep for ${WIKI_UPDATE_SLEEP_SEC} seconds"
     sleep "${WIKI_UPDATE_SLEEP_SEC}"
 done
