3c789f5bf1de0acf5da3231ca047464797fa9fde
Johann Frei <johann.frei@informatik.uni-augsburg.de>
2021-08-03 01:21:51 +0200

    Fix typo

--- dockerized/wikidata-loader/entrypoint.sh
+++ dockerized/wikidata-loader/entrypoint.sh
@@ -7,7 +7,7 @@ echo "Waiting for blazegraph to be available..."
 # Detect initial startup
 if [ "$1" = "/usr/bin/sleep" ] && [ "$2" = "infinity" ]; then
     # Detect enabled update loop
-    if [ -f "/.doUpdate"]; then
+    if [ -f "/.doUpdate" ]; then
         # Async update loop
         echo "UpdateEntrypoint: Start update loop"
         echo "Trigger at: $(date)" >> /updateEntrypoint.log
